4c0d1afc91e14f953de9865698915ea896bbd342
Aaditya1612 <83654180+Aaditya1612@users.noreply.github.com>
2021-07-01 13:06:17 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import base64
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+  background-image: url("data:image/png;base64,%s");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-color: black;
+  width: 650px;
+  padding-top: 0rem;
+  padding-left: 10rem;
+}
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('main2.gif')
+
+components.html('''<html>
+    <body>
+        <style>
+            .rw-conversation-container .rw-message-container {position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%) }
+            .rw-conversation-container .rw-messages-container {background-color: rgb(0,0,0); }
+            .rw-conversation-container .rw-header {background-color: rgb(0, 0, 0);box-shadow: 5px 5px 10px grey;}
+            .rw-conversation-container .rw-messages-container .rw-message .rw-response {background-color: rgba(73, 255, 134, 0.719);color: white;}
+        </style>
+        <script>!(function () {
+            let e = document.createElement("script"),
+              t = document.head || document.getElementsByTagName("head")[0];
+            (e.src =
+              "https://cdn.jsdelivr.net/npm/rasa-webchat/lib/index.js"),
+              // Replace 1.x.x with the version that you want
+              (e.async = !0),
+              (e.onload = () => {
+                window.WebChat.default(
+                  {
+                    outerHeight: "1000px",
+                    title: "Lara",
+                    showFullScreenButton: true,
+                    initPayload: "/greet",
+                    customData: { language: "en" },
+                    socketUrl: "https://b226ca3dda8f.ngrok.io",
+                    // add other props here
+                  },
+                  null
+                );
+              }),
+              t.insertBefore(e, t.firstChild);
+          })();
+          </script>
+          
+    </body>
+</html>''',height=550)
